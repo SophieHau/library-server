048c1934164699bc977de64db6df6591ed365045
sophie <s.hautcoeur@gmail.com>
2020-08-10 15:52:24 +0300

    created routes for get book and author by id and get authors

--- server.js
+++ server.js
@@ -17,10 +17,28 @@ app.listen(port, (error) => {
   console.log('Server running on port ' + port);
 });
 
-app.get('/books', (req, res) => {
+app.get('/books/', (req, res) => {
   res.send(books);
 });
 
-app.get('/authors', (req, res) => {
+app.get('/books/:bookId/', (req, res) => {
+  const { bookId } = req.params;
+  books.forEach((book) => {
+    if (book.id === bookId) {
+      res.json(book);
+    }
+  });
+});
+
+app.get('/authors/', (req, res) => {
   res.send(authors);
+});
+
+app.get('/books/:authorId/', (req, res) => {
+  const { authorId } = req.params;
+  books.forEach((author) => {
+    if (author.id === authorId) {
+      res.json(author);
+    }
+  });
 });
